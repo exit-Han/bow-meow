e238e240893708bece0b911bef24403183621809
Jeonyujin77 <wjsdbwls7777@gmail.com>
2022-11-14 15:35:38 +0900

    project setting

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
 
 client = MongoClient('mongodb+srv://yujin:*RpiZ99tUSxr8UR@cluster0.s5ahq.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
-print('hello')
+# print('hello')
+
+@app.route('/')
+def home():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
