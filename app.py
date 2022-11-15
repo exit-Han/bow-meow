22d6d3a108c6cf8788df4fd95e8b7b4814822ea8
DESKTOP-OP8AFA3\Subin <p.subin2022@gmail.com>
2022-11-15 22:09:47 +0900

    회원가입 html 생성, css 내용 추가, app.py 업데이트

--- app.py
+++ app.py
@@ -88,15 +88,65 @@ def comment_get():
     comment_list = list(db.comment.find({}, {'_id': False}))
     return jsonify({'lists':comment_list})
 
-# 회원가입 페이지 렌더링
-@app.route("/auth/join")
-def join():
-    return render_template('join.html')
 
-# 로그인 페이지 렌더링
-@app.route("/auth/login")
+
+# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
+# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
+SECRET_KEY = 'SPARTA'
+
+# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
+import jwt
+
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
+import datetime
+
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
+import hashlib
+
+
+#################################
+##  HTML을 주는 부분             ##
+#################################
+
+
+
+@app.route('/login')
 def login():
-    return render_template('login.html')
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+#################################
+##  로그인을 위한 API            ##
+#################################
+
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    doc = {
+        "userId": id_receive,  # 아이디
+        "password": pw_hash,  # 비밀번호
+        "nickname": nickname_receive  # 닉네임
+    }
+
+    db.user.insert_one(doc)
+
+    return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
