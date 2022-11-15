43b6f11b5c878f17be66f0a43190ac9046a57725
Jeonyujin77 <wjsdbwls7777@gmail.com>
2022-11-15 15:16:47 +0900

    게시글 등록/리스트 조회/단일조회 API 개발 및 등록화면 개발 1차 #6

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 import os 
 from pymongo import MongoClient
+from bson import json_util, ObjectId
+import json
 
 # load .env
 load_dotenv()
@@ -18,6 +20,68 @@ def home():
 @app.route('/details')
 def detail():
     return render_template('details.html')
+
+# 등록 페이지 렌더링
+@app.route("/view/posts", methods=["GET"])
+def posts_get():
+    return render_template('post.html')
+    
+# [POST] /api/posts
+# {
+#   'id': post_id,
+#   'basicInfo': {
+#     'type': type,
+#     'date': date,
+#     'location': location,
+#     'tel': tel
+#   },
+#   'petInfo': {
+#     'type': type,
+#     'sex': sex,
+#     'age': age,
+#     'weight': weight,
+#     'feature': feature,
+#     'detail': detail
+#   }
+# }
+@app.route("/api/posts", methods=["POST"])
+def posts_post():
+    params = request.get_json() # JSON형식으로 데이터를 받아온다
+    basicInfo = params['basicInfo'] # 신고/제보 관련 기본 정보
+    petInfo = params['petInfo'] # 신고/제보 관련 동물 정보
+
+    try: 
+        doc = {'basicInfo': basicInfo, 'petInfo': petInfo}
+        db.posts.insert_one(doc) # 데이터 등록
+    except:
+        return jsonify({'msg': '등록에 실패하였습니다.'})
+    else:
+        return jsonify({'msg': '등록이 완료되었습니다.'})
+    
+
+# GET /api/posts
+@app.route("/api/posts", methods=["GET"])
+def posts_list_get():
+    page = list(db.posts.find({}))
+    # TypeError: ObjectId('') is not JSON serializable 해결
+    post_list = json.loads(json_util.dumps(page)) 
+    
+    return post_list
+
+
+# GET /api/posts/:id
+@app.route("/api/posts/<string:id>", methods=["GET"])
+def posts_read_get(id):
+    params = request.get_json() 
+    id = params['id']  # 등록된 post의 id
+
+    try:
+        post = db.posts.find_one({'_id': ObjectId(id)})
+        result = json.loads(json_util.dumps(post)) 
+    except:
+        return jsonify({'msg': '조회에 실패하였습니다.'})
+    else:
+        return jsonify(result)
 
 
 if __name__ == '__main__':
