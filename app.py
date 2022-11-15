869108de8cc2b6961441954bc180c3cc77e12171
ptg0811 <ptg0811@gmail.com>
2022-11-15 14:52:15 +0900

    2차 update 입니다. #1 #5

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 import os 
 from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
 
 # load .env
 load_dotenv()
 
 app = Flask(__name__)
 
-client = MongoClient(os.environ.get('DB_URL'))
+client = MongoClient(os.environ.get('DB_URL'), tlsCAFile=ca)
 db = client.sparta
 
 @app.route('/')
@@ -19,6 +22,24 @@ def home():
 def detail():
     return render_template('details.html')
 
+# 댓글기능
+@app.route("/comments", methods=["POST"])
+def comment_post():
+    nickname_receive = request.form['nickname_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'nickname': nickname_receive,
+        'comment': comment_receive,
+    }
+    db.comment.insert_one(doc)
+
+    return jsonify({'msg': '제보 완료!'})
+
+@app.route("/comments", methods=["GET"])
+def comment_get():
+    comment_list = list(db.comment.find({}, {'_id': False}))
+    return jsonify({'lists':comment_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,45 @@
+/*details script*/
+$(document).ready(function () {
+  show_comment();
+});
+
+function save_comment() {
+  let name = $("#name").val();
+  let comment = $("#comment").val();
+
+  $.ajax({
+    type: "POST",
+    url: "/comments",
+    data: { nickname_give: name, comment_give: comment },
+    success: function (response) {
+      alert(response["msg"]);
+      window.location.reload();
+    },
+  });
+}
+
+function show_comment() {
+  $.ajax({
+    type: "GET",
+    url: "/comments",
+    data: {},
+    success: function (response) {
+      let rows = response["lists"];
+      for (let i = 0; i < rows.length; i++) {
+        let nickname = rows[i]["nickname"];
+        let comment = rows[i]["comment"];
+
+        let temp_html = `<div class="card">
+                                            <div class="card-body">
+                                                <blockquote class="blockquote mb-1">
+                                                    <p>${comment}</p>
+                                                    <footer class="blockquote-footer">${nickname}</footer>
+                                                </blockquote>
+                                            </div>
+                                        </div>`;
+        $("#comment-list").append(temp_html);
+      }
+    },
+  });
+}
+/*end of detatils script*/
